d057708e50b5a0160ef984520fda7249e51d0779
Steinshy <nekoheartdev@gmail.com>
2026-04-09 20:16:05 +0200

    chore: Update Ci

--- src/pages/employees/Create.tsx
+++ src/pages/employees/Create.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 import { SuccessModal } from '@/components/Modal';
 import { validateEmployee } from '@/helpers/validator';
